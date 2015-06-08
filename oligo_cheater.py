36fc9b2553f1927220e770ca01fde8f329c88b4f
Bernarda <bernarda@moana.dnsalias.org>
2015-06-08 12:58:33 -1000

    first commit

---
+++ oligo_cheater.py
@@ -0,0 +1,27 @@
+#read in the sequence
+
+sequence = raw_input(Enter your sequence:)
+
+# remove PAM sequence
+
+oligo1=
+
+# add TA to 5' end
+
+oligo1= oligo1
+
+
+# remove GG 
+
+oligo2
+# reverse complement
+
+oligo2=oligo2
+
+# add AAAC to the 5' end
+
+
+print oligo1
+print oligo2
+
+
