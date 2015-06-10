18effe7fe05766375a1013ee4d229100381a3610
Brian Hall <brian@reallymany.com>
2015-06-10 13:17:54 -1000

    removed broken stuff

--- oligo_cheater.py
+++ oligo_cheater.py
@@ -1,27 +1,22 @@
+#!/usr/bin/env python
+
+
 #read in the sequence
 
 sequence = raw_input("Enter your sequence: ")
+print(sequence)
 
 # remove PAM sequence
 
-oligo1=
 
 # add TA to 5' end
-
-oligo1= oligo1
 
 
 # remove GG 
 
-oligo2
+
 # reverse complement
 
-oligo2=oligo2
 
 # add AAAC to the 5' end
-
-
-print oligo1
-print oligo2
-
 
