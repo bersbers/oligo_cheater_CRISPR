f0199c82f1dbae253667b9a3516291c43da0de21
Bernarda <bernarda@mano.localdomain>
2015-06-16 20:39:01 -1000

    now outputs oligo1 and an intermediated oligo

--- oligo_cheater.py
+++ oligo_cheater.py
@@ -13,9 +13,14 @@ print 'trimmed sequence: '+ (trim_seq)
 
 # add TA to 5' end
 
+oligo1 = "TA" + trim_seq 
+print 'oligo 1: ' + (oligo1)
+
 
 # remove GG 
 
+trim2 = trim_seq[2:]
+print trim2
 
 # reverse complement
 
