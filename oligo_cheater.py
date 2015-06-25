21317954dbd2792b62f3b3f70e5f37e9cf09c54e
Bernarda <bernarda@mano.localdomain>
2015-06-24 20:51:25 -1000

    is complete and working now!!

--- oligo_cheater.py
+++ oligo_cheater.py
@@ -9,7 +9,7 @@ print "your target sequence: "+ sequence
 # remove PAM sequence
 
 trim_seq = sequence[:-3]
-print "trimmed sequence: "+ trim_seq
+#print "trimmed sequence: "+ trim_seq
 
 # add TA to 5' end
 
@@ -20,21 +20,32 @@ print "oligo 1: " + oligo1
 # remove GG 
 
 trim2 = trim_seq[2:]
-print trim2
+#print trim2
 
 # reverse trim2
 
 rev_trim2= trim2[::-1]
-print rev_trim2 
+#print rev_trim2 
 
 
 #complement
-
-DNA = {'A':'T', 'C':'G', 'T':'A', 'G':'C'}
-
-for i, j in DNA.iteritems():
-      rev_compl = rev_trim2.replace(i, j)
-print rev_compl   
+compl = ""
+for char in rev_trim2:
+	if char == 'A':
+		compl=compl+'T'
+	if char == 'G':
+		compl=compl+'C'
+	if char == 'C':
+		compl=compl+'G'
+	if char == 'T':
+		compl=compl+'A'
 
 # add AAAC to the 5' end
+
+oligo2="AAAC"+compl
+print "oligo 2: " + oligo2
+
+
+
+
 
